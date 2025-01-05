3646cebfc922e40db4d815945d3d55ce1681dea2
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2025-01-05 21:22:03 +0500

    commit

--- lib/expense.dart
+++ lib/expense.dart
@@ -2,13 +2,16 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
 
-class expense{
-  expense({ required this.title, required this.amount, required this.date}) : id = uuid.v4();
+class expense {
+  expense(this.category,
+      {required this.title, required this.amount, required this.date})
+      : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-
+  final Category category;
 }
