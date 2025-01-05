f8261f115247740074339c3fb699ca9ae9235eaa
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2025-01-05 22:05:07 +0500

    commit

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,9 +1,5 @@
-
-
-
+import 'package:expense_tracker_app/expense.dart';
 import 'package:flutter/material.dart';
-
-
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -13,10 +9,28 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<expense> _registeredExpense = [
+    expense(
+        category: Category.work,
+        title: "Flutter Course",
+        amount: 19.99,
+        date: DateTime.now()),
+    expense(
+        category: Category.leisure,
+        title: "Python Course",
+        amount: 15.99,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [
-      Text("Chart")
-    ],),);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [Text("Chart")],
+      ),
+    );
   }
 }
