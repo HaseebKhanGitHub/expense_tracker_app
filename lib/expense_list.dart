12d605426196af1e40ae6fb279f3358c55419ea3
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2025-01-06 14:54:25 +0500

    commit

---
+++ lib/expense_list.dart
@@ -0,0 +1,13 @@
+import 'package:expense_tracker_app/expenses.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseList extends StatelessWidget {
+  const ExpenseList({super.key, required this.expenses});
+
+  final List<Expenses> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
