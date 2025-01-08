7b69519e8a4a2c843f663b8185d8a9d9ed5583bf
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2025-01-09 00:04:10 +0500

    commit

--- lib/widgets/expense_item.dart
+++ lib/widgets/expense_item.dart
@@ -1,10 +1,14 @@
+import 'package:expense_tracker_app/model/expense.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ExpenseItem extends StatelessWidget {
-  const ExpenseItem({super.key});
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(child: Text(expense.title));
   }
 }
