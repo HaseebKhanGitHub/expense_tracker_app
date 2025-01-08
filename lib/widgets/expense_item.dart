335689b6dd61949282cca35c2cdf53285bea9911
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2025-01-08 23:59:30 +0500

    commit

---
+++ lib/widgets/expense_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
